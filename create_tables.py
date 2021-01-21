87aa9d5874d9508b1707cb9bff09b21a995650b4
Omar <omarfessy@gmail.com>
2021-01-21 11:04:46 +0100

    Connect and create sparkifydb

--- create_tables.py
+++ create_tables.py
@@ -2,6 +2,25 @@
 import psycopg2 # python driver to set connexion to Postgres
 def create_database():
 	#connect to the default database :
-	conn=psycopg2.connect(host="127.0.0.1 dbname=postgres user=postgres password=admin" )
+	conn=psycopg2.connect("host=127.0.0.1 dbname=postgres user=postgres password=admin" )
 	conn.set_session(autocommit=True)
 	cur=conn.cursor()
+
+	#drop sparkifydb if it exists 
+	cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+	#create sparkifydb where to host all tables 
+	cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+
+	#close the connection to the default database
+	conn.close()
+
+
+
+def main():
+	create_database()
+
+if __name__=="__main__":
+	main()
+
+
+

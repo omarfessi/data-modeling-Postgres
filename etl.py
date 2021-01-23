06ed3c5ff9a3f3c4bd5e39b052a6948da00f4cc6
Omar <omarfessy@gmail.com>
2021-01-23 11:12:11 +0100

    Add docstrings

--- etl.py
+++ etl.py
@@ -6,6 +6,17 @@ from sql_queries import *
 
 
 def process_song_data(cur, filepath):
+	"""
+    Execute the song_table_insert & artist_table_insert query 
+    after reading json files and extracting the fields needed to populate the related tables.
+            Parameters:
+                cur :a cursor to execute the CRUD statements via the connection to the database
+                filepath: string that indictes the filepath directory in order to open the file
+            Returns:
+                dataframe containing the json records
+                insert song record
+                insert artist record
+    """
 	df = pd.read_json(filepath, lines=True)
 	song_data = df[["song_id", "title", "artist_id", "year", "duration"]].values[0]
 	cur.execute(song_table_insert, song_data.tolist())
@@ -14,6 +25,19 @@ def process_song_data(cur, filepath):
 	cur.execute(artist_table_insert, artist_data.tolist())
 
 def process_log_data(cur, filepath):
+	"""
+    Open the log file, filter by NextSong action, convert timestamp column to datetime, 
+    insert time data records into the time table,
+    extract the fields needed to populate the user table, and songplay table
+   
+            Parameters:
+                cur :a cursor to execute the CRUD statements via the connection to the database
+                filepath: string that indictes the filepath directory in order to open the log file
+            Returns:
+                dataframe containing the json records ( time_df, user_data, songplay_data)
+                insert records into user table 
+                insert records into songplay table
+    """
 	df = pd.read_json(filepath, lines=True)
 	df = df[df['page'] == 'NextSong']
 	t=pd.to_datetime(df['ts'],unit='ms')
@@ -51,6 +75,15 @@ def process_log_data(cur, filepath):
 		cur.execute(songplay_table_insert, songplay_data)
 
 def process_data(cur, conn, filepath, func):
+	""" 
+	 get all files matching extension from directory and iterate over them and process
+            parameters: 
+                cur :a cursor to execute the CRUD statements via the connection to the database
+                filepath: string that indictes the filepath directory in order to open the log file
+                conn: the connection to the database
+                func : is the function needed to process the related file ( song_file or log_file)
+        
+    """
     all_files=[]
     for root, dirs, files in os.walk(filepath):
         files=glob.glob(os.path.join(root,'*.json'))
@@ -66,6 +99,15 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+	 """
+	creates the connection to the database and creates a cursor as well
+    and process both nature of data ( song_file & log_file)
+    and close connection
+        parameters: 
+            None
+        returns :
+            None
+    """
 	conn=psycopg2.connect("host=127.0.0.1 user=postgres password=admin dbname=sparkifydb")
 	cur=conn.cursor()
 
